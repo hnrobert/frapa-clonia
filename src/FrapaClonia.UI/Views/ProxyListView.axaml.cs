1a9ed3e8bd03204f298eb66b748b425eff2c839f
Robert He <hnrobert@qq.com>
2026-02-07 01:25:14 +0800

    fix: update layout, run normally

--- src/FrapaClonia.UI/Views/ProxyListView.axaml.cs
+++ src/FrapaClonia.UI/Views/ProxyListView.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using FrapaClonia.Domain.Models;
+using FrapaClonia.UI.ViewModels;
 
 namespace FrapaClonia.UI.Views;
 
@@ -7,5 +10,44 @@ public partial class ProxyListView : UserControl
     public ProxyListView()
     {
         InitializeComponent();
+    }
+
+    private void OnEditClick(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button button && button.DataContext is ProxyConfig proxy &&
+            DataContext is ProxyListViewModel viewModel)
+        {
+            viewModel.SelectedProxy = proxy;
+            if (viewModel.EditProxyCommand.CanExecute(null))
+            {
+                viewModel.EditProxyCommand.Execute(null);
+            }
+        }
+    }
+
+    private void OnDuplicateClick(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button button && button.DataContext is ProxyConfig proxy &&
+            DataContext is ProxyListViewModel viewModel)
+        {
+            viewModel.SelectedProxy = proxy;
+            if (viewModel.DuplicateProxyCommand.CanExecute(null))
+            {
+                viewModel.DuplicateProxyCommand.Execute(null);
+            }
+        }
+    }
+
+    private void OnDeleteClick(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button button && button.DataContext is ProxyConfig proxy &&
+            DataContext is ProxyListViewModel viewModel)
+        {
+            viewModel.SelectedProxy = proxy;
+            if (viewModel.DeleteProxyCommand.CanExecute(null))
+            {
+                viewModel.DeleteProxyCommand.Execute(null);
+            }
+        }
     }
 }
