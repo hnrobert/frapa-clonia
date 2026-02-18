15fdbf8afd57469c6bf1272f59968e4184590774
Robert He <hnrobert@qq.com>
2026-02-19 00:46:57 +0800

    fix: clean up unused strings

--- src/FrapaClonia.UI/Views/FrpcConfigurationDialog.axaml.cs
+++ src/FrapaClonia.UI/Views/FrpcConfigurationDialog.axaml.cs
@@ -5,7 +5,7 @@ namespace FrapaClonia.UI.Views;
 
 public partial class FrpcConfigurationDialog : Window
 {
-    private FrpcConfigurationDialog()
+    public FrpcConfigurationDialog()
     {
         InitializeComponent();
     }
